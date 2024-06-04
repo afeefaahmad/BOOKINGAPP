f5488788a662373f19e50044cc1f53b0ec79e6a3
afeefaahamd <goodafeefa@gmail.com>
2024-06-04 08:42:47 +0530

    CREATE HOTELS api made.

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+app.use(express.json())
 
-app.use("api/auth", authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+
 
 app.listen(8800, () => {
   connect();
