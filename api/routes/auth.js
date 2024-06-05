063b4e3a2e20d7d5fcd8a5b7e3cd9b2261c57f9b
afeefaahamd <goodafeefa@gmail.com>
2024-06-05 06:56:15 +0530

    Register and Login completed.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
+import {register} from "../controllers/auth.js";
+import {login} from "../controllers/auth.js"
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("hello from auth");
-  })
+router.post("/register", register)
+router.post("/login", login)
 
 export default router
